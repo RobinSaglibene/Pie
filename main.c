965088fd19a3be22b03f54e633dbe6ebef4771cb
vboxuser <vboxuser@Debian1.myguest.virtualbox.org>
2022-11-21 12:25:27 +0100

    main modified

--- main.c
+++ main.c
@@ -102,15 +102,16 @@ int main( int argc, char *argv[]) {
 
   char command[32];
   char data[1024];
-  //sprintf(command, "display %s",filepng);
+  sprintf(command, "display %s",filepng);
   //Open the process with given 'command' for reading
+  /*
   FILE* file = popen(command, "r");
   // do something with program output.
   while (fgets(data, sizeof(data)-1, file) != NULL)
   {
       printf("%s", data);
   }
-  pclose(file);
+  pclose(file);*/
   return(0);
 }
 
